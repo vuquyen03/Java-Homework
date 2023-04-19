19800b4dcdf7495fa9cc93d11f976e71f273dc0a
vuquyen03 <90053705+vuquyen03@users.noreply.github.com>
2023-04-19 11:41:37 +0700

    Add files via upload

---
+++ SortArray.java
@@ -0,0 +1,25 @@
+package day;
+import java.util.Scanner;
+public class SortArray {
+	public static void main(String[] argv) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		for(int i = 0; i<size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i = 0; i<size; i++) {
+			for(int j = 0; j<size - 1; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp;
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i = 0; i<size;i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
