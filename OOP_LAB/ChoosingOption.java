feba43527d113f0ad7df4552649fefe26a4ce3d3
vuquyen03 <90053705+vuquyen03@users.noreply.github.com>
2023-05-06 08:18:29 +0700

    Update ChoosingOption.java

--- OOP_LAB/ChoosingOption.java
+++ OOP_LAB/ChoosingOption.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class ChoosingOption {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int option = JOptionPane.showConfirmDialog(null, 
 								"Do you want to change to the first class ticket?");
 		JOptionPane.showMessageDialog(null, "You've chosen: "
