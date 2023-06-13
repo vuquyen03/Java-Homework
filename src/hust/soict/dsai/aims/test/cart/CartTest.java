8ddcd67e7c9baabf46c40b5b52f57594dc804a17
vuquyen03 <quyenvungoc1711@gmail.com>
2023-06-14 02:53:10 +0700

    lab oop4

---
+++ src/hust/soict/dsai/aims/test/cart/CartTest.java
@@ -0,0 +1,27 @@
+package hust.soict.dsai.aims.test.cart;
+
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+
+import java.util.Scanner;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+
+        cart.addMedia(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addMedia(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+        cart.addMedia(dvd3);
+
+        cart.print();
+
+    }
+}
