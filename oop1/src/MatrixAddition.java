6a690f5878b49d4e2265dc1d183c05e45dc7ce3f
vuquyen03 <quyenvungoc1711@gmail.com>
2023-06-16 20:45:35 +0700

    Done

---
+++ oop1/src/MatrixAddition.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = input.nextInt();
+
+        int[][] firstMatrix = new int[rows][cols];
+        int[][] secondMatrix = new int[rows][cols];
+
+        System.out.println("Enter the elements of the first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                firstMatrix[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("Enter the elements of the second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                secondMatrix[i][j] = input.nextInt();
+            }
+        }
+
+        // Add the two matrices
+        int[][] sumMatrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
+
+        System.out.println("The sum of the two matrices is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sumMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
---
+++ oop1/src/PrintStar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PrintStar {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for (int j = 1; j<2*n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j<2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
